3617d2d2210d83a89502a2aee8d03c08d066f82b
Awais Rana <awaisranafastian@gamil.com>
2020-08-28 20:55:16 +0500

    touchable component updated

--- App/Components/touchable/touchable.js
+++ App/Components/touchable/touchable.js
@@ -1,6 +1,6 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { TouchableOpacity } from 'react-native';
-import PropTypes from 'prop-types';
 
 const Touchable = (props) => {
   return (
